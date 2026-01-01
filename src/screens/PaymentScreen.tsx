a5ff68ef3aca60a0711925a4539f2ae522703bc9
Saint. Clever <parchment@gmx.com>
2025-12-31 22:33:01 -0500

    Create + ADD NEW button

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -29,6 +29,29 @@ const PaymentScreen = () => {
       <PaymentList />
       <View style={{ paddingHorizontal: s(16) }}>
         <BankCardBox />
+        <View
+          style={{
+            marginTop: vs(15),
+            width: s(327),
+            height: vs(62),
+            borderRadius: s(10),
+            borderWidth: 2,
+            borderColor: "#F0F5FA",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "semibold",
+              fontSize: s(14),
+              color: "#FF7622",
+              textAlign: "center",
+            }}
+          >
+            + ADD NEW
+          </Text>
+        </View>
       </View>
     </View>
   );
